c519ed453fd3996c5a2cd34005060d922ea84c0a
Rasso Hilber <mail@rassohilber.com>
2025-10-30 16:43:04 +0100

    Fix lintstaged for php files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 export default {
   "**/*.{js,jsx,mjs,cjs,ts,mts,css,scss}": ["prettier --write"],
-  "**/*.php": ["pnpm run format:php && pnpm run analyse"],
+  "**/*.php": ["pnpm run format:php", "pnpm run analyse"],
 };
